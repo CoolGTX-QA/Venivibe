15297ef4f3ec9d53544f086df4b4305732705e17
singhkuljeet <singhkuljeet@bugraptors.com>
2025-11-07 17:45:25 +0530

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,38 +4,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
-    public void chromedrive(){
+    public void chromedrive() {
         ChromeDriver driver = new ChromeDriver();
-    public static void main (String[] args)
-    {
-        String greeting = "Hello and welcome!";
-        int number = 5;
-        boolean isActive = true;
-        double pi = 3.14;
-        char letter = 'A';
-        // This is a single-line comment
+        {
+            String greeting = "Hello and welcome!";
+            int number = 5;
+            boolean isActive = true;
+            double pi = 3.14;
+            char letter = 'A';
+            // This is a single-line comment
 
-        System.out.println(greeting);
-        System.out.println("Number: " + number);
-        System.out.println("Is Active: " + isActive);
-        System.out.println("Pi: " + pi);
-        System.out.println("Letter: " + letter);
-
+            System.out.println(greeting);
+            System.out.println("Number: " + number);
+            System.out.println("Is Active: " + isActive);
+            System.out.println("Pi: " + pi);
+            System.out.println("Letter: " + letter);
 
 
-
-        public void chromedrive(){
-        ChromeDriver driver = new ChromeDriver();
-
-
-        //this is a Comment example
+//        public void chromedrive(){
+//        ChromeDriver driver = new ChromeDriver();
 
 
-    System.out.println("Hello World");
-    Greeting greeting =new Greeting();
-    greeting.hello();
-    Messenger Messanger = new Messenger();
-    Messanger.message();
+            //this is a Comment example
+//
+//
+//    System.out.println("Hello World");
+//    Greeting greeting =new Greeting();
+//    greeting.hello();
+//    Messenger Messanger = new Messenger();
+//    Messanger.message();
+//
+////    }
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Helloo");
     }
 }
